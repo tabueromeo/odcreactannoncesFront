b0f807c072abf32ca9399fbf61b2f55517942859
NGANKAM Perlis <perlis.ngankam2@gmail.com>
2021-04-30 17:33:16 +0000

    modifier un user

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+//import Login from './component/Users/Login';
+import Adduser from './component/Users/Adduser';
+
 
 function App() {
   return (
     <div className="App">
       <p>hello les geeks</p>
+     
+
     </div>
   );
 }
--- src/component/Main.js
+++ src/component/Main.js
@@ -1,13 +1,21 @@
-
 import App from "../App"
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './Users/Login';
+import Adduser from './Users/Adduser';
+
 
 function Main(){
     return  <BrowserRouter>
         <Switch>
          
-        <Route exact path="/">
-          <App/>
+          <Route exact path="/">
+            <App/>
+          </Route>
+          <Route  path="/Login">
+            <Login/>
+          </Route>
+            <Route  path="/Adduser">
+            <Adduser/>
           </Route>
          
         </Switch>
